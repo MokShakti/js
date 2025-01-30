b96e184f6f05a1e93bae57d1d1039f66cf72e424
MokShakti <167544238+MokShakti@users.noreply.github.com>
2025-01-30 17:38:01 +0100

    Add files via upload

--- lesson_03/scripts/task_02.js
+++ lesson_03/scripts/task_02.js
@@ -4,11 +4,6 @@ const unitPrice = parseFloat(prompt(`Enter unit's price`))
 const amountOfMoney = parseFloat(prompt(`Enter your amount of money`))
 const change = amountOfMoney - unitPrice
 
-//Обчислення
-if (isNaN(unitPrice) || isNaN(amountOfMoney)) {
-   document.write(`Error: Enter the amount`)
-}
-
 switch (true) {
    case change < 0:
       document.write(`Not enough money to initiate transaction`)
@@ -19,4 +14,6 @@ switch (true) {
    case change >= 4:
       document.write(`Transaction is successful. The change is ${change.toFixed(2)}zl. You have enough money to purchase our daily lottery for 4zl. Would you like to?`)
       break;
+   default:
+      document.write(`Error: Enter the amount`)
 }
